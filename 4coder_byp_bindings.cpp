17021a3afb37cf0ecfeab16bc947068454f0795b
BYP <bypan00@gmail.com>
2022-01-20 22:13:35 -0500

    vim updates

--- 4coder_byp_bindings.cpp
+++ 4coder_byp_bindings.cpp
@@ -6,7 +6,7 @@ byp_essential_mapping(Mapping *mapping, i64 global_id, i64 file_id, i64 code_id)
 
 	SelectMap(global_id);
 	BindCore(byp_startup, CoreCode_Startup);
-	BindCore(default_try_exit, CoreCode_TryExit);
+	BindCore(vim_try_exit, CoreCode_TryExit);
 	BindCore(clipboard_record_clip, CoreCode_NewClipboardContents);
 	BindMouseWheel(mouse_wheel_scroll);
 	BindMouseWheel(mouse_wheel_change_face_size, KeyCode_Control);
